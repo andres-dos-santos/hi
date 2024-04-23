1c58681dc5c6481151aa21defa83f332f4de914e
Desenvolvimento <desenvolvimento@zaal.com.br>
2024-04-22 23:31:48 -0300

    feat: add logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,40 +39,12 @@ export default function RootLayout({
                 <div className="flex items-center space-x-2.5 sm:space-x-5">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
                     width="24"
                     height="24"
+                    viewBox="0 0 256 256"
+                    className="fill-zinc-800 dark:fill-white"
                   >
-                    <path
-                      d="M9.01654 6.15879C10.9944 4.77262 17.9171 3.55944 18.906 6.15879C20.3862 10.0497 3.87743 12.3805 5.06077 6.15849C5.55508 3.55944 10.5002 2 12.9725 2"
-                      stroke="currentColor"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                    />
-                    <path
-                      d="M18 11.1934C15.423 13.0706 8.5771 13.8244 6 11.7816"
-                      stroke="currentColor"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                    />
-                    <path
-                      d="M14.0219 21.6941C13.0436 21.8816 12.0077 21.989 11 21.9995"
-                      stroke="currentColor"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                    />
-                    <path
-                      d="M7 15.166C9.07731 16.0444 12.3835 15.9574 15 15.2812"
-                      stroke="currentColor"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                    />
-                    <path
-                      d="M8.03906 18.5039C9.49304 18.8598 11.2867 18.8854 12.9988 18.6635"
-                      stroke="currentColor"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                    />
+                    <path d="M215.79,118.17a8,8,0,0,0-5-5.66L153.18,90.9l14.66-73.33a8,8,0,0,0-13.69-7l-112,120a8,8,0,0,0,3,13l57.63,21.61L88.16,238.43a8,8,0,0,0,13.69,7l112-120A8,8,0,0,0,215.79,118.17ZM109.37,214l10.47-52.38a8,8,0,0,0-5-9.06L62,132.71l84.62-90.66L136.16,94.43a8,8,0,0,0,5,9.06l52.8,19.8Z"></path>
                   </svg>
 
                   <span className="after:content-['@andres'] sm:after:content-['@andresdossantos'] text-sm font-medium dark:text-zinc-400 text-zinc-700" />
