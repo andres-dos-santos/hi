549caedf5e56820abfa43914d5fc7e82bee3b73c
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2024-10-21 15:36:58 -0300

    minimalist, just important thing

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,7 @@
 import type { Metadata } from 'next'
 import { Inter, IBM_Plex_Mono } from 'next/font/google'
 
-import { Providers } from './providers'
-
-// import { Theme } from '@/components/theme'
-// import { Logo } from '@/components/logo'
-
 import './globals.css'
-import { Theme } from '@/components/theme'
-import { Link } from '@/components/pathname'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 const plexMono = IBM_Plex_Mono({
@@ -32,56 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${plexMono.variable}`}>
       <body className="antialiased bg-[#fefefe] dark:bg-[#1c1c1c] px-10 sm:px-0">
-        <Providers>
-          <header className="h-32 flex items-center bg-zinc-800/50 fixed top-0 left-0 right-0 backdrop-blur-xl">
-            <div className="sm:w-[900px] sm:mx-auto flex items-center justify-between gap-2.5">
-              <div className="h-14 w-14 rounded bg-zinc-700/50 flex items-center justify-center">
-                <span className="font-semibold font-mono text-sm -tracking-wide">
-                  AS
-                </span>
-              </div>
-
-              <div className="block">
-                <h1 className="text-xs font-extrabold -tracking-wide">
-                  <span className="text-lg font-semibold">A</span>NDRES{' '}
-                  <span className="text-lg font-semibold">D</span>OS{' '}
-                  <span className="text-lg font-semibold">S</span>ANTOS
-                </h1>
-              </div>
-
-              <nav className="ml-auto flex items-center">
-                <span className="text-[11px] font-semibold tracking-wide">
-                  HOME
-                </span>
-
-                <span className="text-[11px] font-semibold tracking-wide mx-5">
-                  PROJECTS
-                </span>
-
-                <span className="text-[11px] font-semibold tracking-wide">
-                  ARTICLES
-                </span>
-              </nav>
-            </div>
-          </header>
-          <div className="sm:max-w-[900px] sm:mx-auto">{children}</div>
-
-          <footer className="h-14 border-t border-zinc-200 dark:border-zinc-800 absolute right-0 left-0 w-full flex items-center justify-between">
-            <div className="sm:w-[900px] sm:mx-auto flex items-center justify-between gap-2.5">
-              <button>
-                <span className="text-xs font-medium text-zinc-600 dark:text-zinc-300 hover:text-zinc-900 dark:hover:text-white">
-                  Made with <span className="underline">NextJS</span>
-                </span>
-              </button>
-
-              <button>
-                <span className="text-xs font-medium text-zinc-600 dark:text-zinc-300 hover:text-zinc-900 dark:hover:text-white">
-                  Share feedback
-                </span>
-              </button>
-            </div>
-          </footer>
-        </Providers>
+        <div className="sm:max-w-[700px] sm:mx-auto h-screen flex items-center">
+          {children}
+        </div>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,186 +1,74 @@
-import { HomeFooter } from '@/components/home-footer'
-import { Map } from '@/components/map'
-import { SayHi } from '@/components/say-hi'
-import Image from 'next/image'
+import Link from 'next/link'
 
-export const revalidate =
-  process.env.NODE_ENV === 'development' ? 30 : 60 * 60 * 1 // 1 hour
+// export const revalidate =
+//   process.env.NODE_ENV === 'development' ? 30 : 60 * 60 * 1 // 1 hour
 
-interface Post {
-  title: string
-  id: string
-  createdAt: string
-}
+// interface Post {
+//   title: string
+//   id: string
+//   createdAt: string
+// }
 
-const queryWithoutSearches = `
-  query {
-    posts(orderBy: createdAt_DESC) {
-      title
-      id
-      createdAt
-    }
-  }
-`
+// const queryWithoutSearches = `
+//   query {
+//     posts(orderBy: createdAt_DESC) {
+//       title
+//       id
+//       createdAt
+//     }
+//   }
+// `
 
-const queryWithSearches = `
-  query GetLearnPosts($searched: String) {
-    posts(where: { title_contains: $searched }, orderBy: createdAt_DESC) {
-      title
-      id
-      createdAt
-    }
-  }
-`
+// const queryWithSearches = `
+//   query GetLearnPosts($searched: String) {
+//     posts(where: { title_contains: $searched }, orderBy: createdAt_DESC) {
+//       title
+//       id
+//       createdAt
+//     }
+//   }
+// `
 
-async function getPosts(searched?: string): Promise<Post[]> {
-  const payload = searched
-    ? { query: queryWithSearches, variables: { searched } }
-    : { query: queryWithoutSearches }
+// async function getPosts(searched?: string): Promise<Post[]> {
+//   const payload = searched
+//     ? { query: queryWithSearches, variables: { searched } }
+//     : { query: queryWithoutSearches }
 
-  const response = await fetch(
-    'https://api-sa-east-1.hygraph.com/v2/clcckqn423yp601uo3xcd1rh7/master',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    },
-  )
+//   const response = await fetch(
+//     'https://api-sa-east-1.hygraph.com/v2/clcckqn423yp601uo3xcd1rh7/master',
+//     {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(payload),
+//     },
+//   )
 
-  const json = await response.json()
+//   const json = await response.json()
 
-  return json.data ? json.data.posts : []
-}
+//   return json.data ? json.data.posts : []
+// }
 
-export default async function Learns(props: {
-  searchParams?: { find: string }
-}) {
-  const posts = await getPosts(props.searchParams?.find)
-
+export default async function Learns() {
   return (
-    <div className="px-10 pb-20">
-      {/* <div className="relative">
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id}>
-              <Link
-                className="flex items-start gap-x-5 sm:gap-x-10 group mb-5"
-                href={`/post/${post.id}`}
-              >
-                <span className="text-sm text-zinc-700 font-medium dark:font-normal dark:text-white group-hover:underline">
-                  {post.title}
-                </span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div> */}
-
-      <div className="flex items-baseline mb-8 mt-[13rem] gap-5">
-        <h6 className="first-letter:text-lg first-letter:font-semibold text-xs font-extrabold -tracking-wide">
-          ABOUT ME
-        </h6>
-        <span className="text-[12px] font-semibold text-zinc-400 uppercase">
-          4+ years of experience with Next, React Native, Node or Android
-        </span>
+    <div className="">
+      <div className="flex items-baseline mb-8 mt-10 gap-5">
+        <h6 className="font-medium -tracking-wider">Hello!</h6>
       </div>
 
-      <SayHi />
-
-      <span className="text-sm block mb-10 leading-7">
-        I am a developer passionate about technology, always looking for new
-        challenges and learning. With experience in several programming
-        languages​ ​(such as JavaScript and Kotlin) and frameworks (such as
-        React/React Native/Node and Jetpack Compose), I stand out for my ability
-        to adapt efficient and creative solutions to solve complex problems.
+      <span className="text-sm block mb-10 leading-7 -tracking-wide">
+        My name is Andres, I am a programmer specialized in React and React
+        Native, very good with Node and learning Android, contact me on{' '}
+        <Link href="http://linkedin.com/in/andres-dosantos/" target="__blank">
+          LinkedIn
+        </Link>
+        .
       </span>
 
-      <span className="text-sm block mb-10 leading-7">
-        I have solid knowledge in Object-Oriented Programming, mobile
-        application development, and have worked with tools such as Supabase,
-        Firebase, Styled Components, AWS, Jetpack Compose, NextJS and Expo.
+      <span className="text-sm block mb-10 leading-7 -tracking-wide">
+        I have 1 published application so far.
       </span>
-
-      <span className="text-sm block mb-10 leading-7">
-        I'm currently exploring areas such as AI applied to medicine, as well as
-        expanding my skills in iOS development.
-      </span>
-
-      <span className="text-sm block mb-10 leading-7">
-        My approach to development is centered on clean code, modularity, and
-        good design practices. I enjoy collaborating in multidisciplinary teams,
-        where I can share knowledge and learn from other professionals. I am
-        interested in opportunities that allow me to continue to evolve as a
-        developer, while contributing to innovative and impactful projects.
-      </span>
-
-      <span className="text-sm block mb-16 leading-7">
-        Whether developing a project from scratch or optimizing existing
-        systems, I am always ready to take on the next challenge and create
-        solutions that make a difference.
-      </span>
-
-      {/* <span className="text-[13px] block mb-10">mapa de onde eu moro</span> */}
-
-      {/* <Map /> */}
-
-      <h6 className="first-letter:text-lg first-letter:font-semibold text-xs font-extrabold mb-16 -tracking-wide">
-        ABOUT MY WORK
-      </h6>
-
-      <div className="flex items-center gap-5 mb-10">
-        <Image src="/h4.png" alt="" width={35} height={35} />
-        <div className="flex flex-col">
-          <span className="font-semibold text-sm mb-0.5">H4money</span>
-          <span className="text-xs text-zinc-300 font-medium">
-            JR Developer | Aqui fui responsável por elaborar junto com o time um
-            sistema de gestão financeira com React e React Native.
-          </span>
-        </div>
-      </div>
-
-      <div className="flex items-center gap-5 mb-16">
-        <Image
-          src="/zaal.webp"
-          alt=""
-          width={35}
-          height={35}
-          className="mb-auto"
-        />
-        <div className="flex flex-col">
-          <span className="font-semibold text-sm mb-0.5">Zaal</span>
-          <span className="text-xs text-zinc-300 font-medium">
-            PL Developer | Aqui estou responsável por:
-          </span>
-
-          <span className="text-xs font-medium flex mb-2.5 mt-5">
-            - Aplicativo de cashback: um aplicativo para mostrar os anúncios e
-            os códigos promocionais da empresa selecionada.
-          </span>
-          <span className="text-xs font-medium flex mb-2.5">
-            - Site do cashback: um site completo para empresas enviarem anúncios
-            para o aplicativo.
-          </span>
-          <span className="text-xs font-medium flex mb-2.5">
-            - Aplicativo de resultado: um aplicativo para mostrar os resultados
-            da sua empresa, com marcações gráficas e listas.
-          </span>
-          <span className="text-xs font-medium flex mb-2.5">
-            - Aplicativo de MultiPDV: um aplicativo para ajudar vendedores no
-            dia a dia das vendas, enviando o comprovante para o caixa.
-          </span>
-          <span className="text-xs font-medium flex mb-2.5">
-            - Site de ERP: site ERP com integração com notas fiscais.
-          </span>
-        </div>
-      </div>
-
-      <h6 className="first-letter:text-lg first-letter:font-semibold text-xs font-extrabold mb-16 -tracking-wide">
-        WHERE DO I HIDE
-      </h6>
-
-      <Map />
     </div>
   )
 }
