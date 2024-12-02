162d693a40589cb913706c8d32904cd2714dd1e4
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2024-12-01 21:47:57 -0300

    feat: add grid

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${inter.variable} ${plexMono.variable}`}>
+    <html
+      lang="en"
+      className={`${inter.variable} ${plexMono.variable} relative`}
+    >
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#9090900a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_14px]"></div>
+      {/* <div className="absolute left-0 right-0 bottom-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div> */}
       <body className="antialiased bg-[#fefefe] dark:bg-[#1c1c1c] px-10 sm:px-0">
         <main className="mx-auto max-w-[800px]">{children}</main>
       </body>
