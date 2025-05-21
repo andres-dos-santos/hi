580f8bd76a3ec759320686b6e0ab1f61c78cfe2e
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2025-05-21 09:40:36 -0300

    style(dots): change dots from text to div

--- components/dots.tsx
+++ components/dots.tsx
@@ -1,3 +1,13 @@
 export function Dots() {
-	return <span className="my-20 block text-[13px] text-zinc-700/85">. . .</span>
+	return (
+		<div className="my-20">
+			{Array.from({ length: 3 }, (_, i) => (
+				<span
+					// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+					key={i}
+					className="inline-block h-[2px] w-[2px] rounded-full bg-zinc-500/85 ring-1 ring-zinc-700/10 mr-2"
+				/>
+			))}
+		</div>
+	)
 }
